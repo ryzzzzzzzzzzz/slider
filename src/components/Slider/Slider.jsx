1238a8a9c56fe6892eabdd519952509d759c9830
ryz <knyazevskaya.a.a@gmail.com>
2025-02-28 16:20:04 +0300

    add css for 1st slide

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -14,7 +14,7 @@ const Slider = () => {
     const preventScroll = usePreventScroll();
 
     const slides = [
-        {img: bg1, heading: 'Привет,', block: 'Это не коммерческое задание'},
+        {img: bg1, heading: 'Привет,', block: (<span>Это <b>не</b><br/>коммерческое<br/>задание</span>), btn: 'Что дальше?'},
         {img: bg2, heading: 'Текст сообщения', largeBlock: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis ut diam quam nulla, Mauris in aliquam sem fringilla ut morbi tincidunt. ' +
                 'Vitae aliquet nec ullamcorper sit amet risus nullam eget felis. Nulla pharetra diam sit amet nisl. Eget nulla facilisi etiam dignissim diam quis enim lobortis. Est sit amet facilisis magna. Neque laoreet suspend sse interdum consectet r libero id. ' +
                 'Nec ullamcorper sit amet risus nullam eget felis eget. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id Euismod quis viverra nibh cras pulvinar mattis nunc. Massa massa ultricies mi quis, Sit amet massa vitae tortor condimentum lacinia. ' +
@@ -117,11 +117,16 @@ const Slider = () => {
                         style={{backgroundImage: `url(${slide.img})`}}
                     >
                         <div className={s.content}>
-                            <h1>{slide.heading}</h1>
-                            <h2>{slide.block}</h2>
-                            <p>{slide.largeBlock}</p>
-                            <p>{slide.smallBlock1}</p>
-                            <p>{slide.smallBlock2}</p>
+                            <div className={s.blocks}>
+                                <h1>{slide.heading}</h1>
+                                <h2>{slide.block}</h2>
+                                {slide.btn &&
+                                    <button className={s.btn}><div className={s.arrow}>&rarr;</div><span>{slide.btn}</span></button>
+                                }
+                                <p>{slide.largeBlock}</p>
+                                <p>{slide.smallBlock1}</p>
+                                <p>{slide.smallBlock2}</p>
+                            </div>
                         </div>
                     </div>
                 ))}
@@ -131,152 +136,3 @@ const Slider = () => {
 };
 
 export default Slider;
-
-// const Slider = () => {
-//     const [currentSlide, setCurrentSlide] = useState(0);
-//     const slideRef = useRef(null);
-//     const [touchStartX, setTouchStartX] = useState(0);
-//     const [touchStartY, setTouchStartY] = useState(0);
-//     const [isDragging, setIsDragging] = useState(false);
-//     const preventScroll = usePreventScroll();
-//     const isPreventingScroll = preventScroll.current;
-//     const [isChangingSlide, setIsChangingSlide] = useState(false);
-//
-//     const slides = [
-//         {img: bg1, heading: '1'},
-//         {img: bg2, heading: '2'},
-//         {img: bg3, heading: '3'},
-//     ];
-//
-//     const changeSlide = (newSlide) => {
-//         setIsChangingSlide(true);
-//         setTimeout(() => {
-//             setCurrentSlide(newSlide);
-//             setIsChangingSlide(false);
-//         }, 200); // задержка в 200мс
-//     };
-//
-//     // Применяем хук useTimeout
-//     useTimeout(() => {
-//         // Здесь можно добавить дополнительные действия
-//     }, 200);
-//
-//     useEffect(() => {
-//         const handleScroll = (e) => {
-//             e.preventDefault();
-//         };
-//         document.body.addEventListener('touchmove', handleScroll, { passive: false });
-//         document.body.addEventListener('mousemove', handleScroll, { passive: false });
-//
-//         return () => {
-//             document.body.removeEventListener('touchmove', handleScroll);
-//             document.body.removeEventListener('mousemove', handleScroll);
-//         };
-//     }, []);
-//
-//     const handleMouseDown = (e) => {
-//         setTouchStartX(e.clientX);
-//         setTouchStartY(e.clientY);
-//         setIsDragging(true);
-//     };
-//
-//     const handleMouseMove = (e) => {
-//         if (!isDragging) return;
-//         const currentX = e.clientX;
-//         const diffX = currentX - touchStartX;
-//
-//         if (Math.abs(diffX) > 40) {
-//             const direction = diffX > 0 ? 1 : -1;
-//             setCurrentSlide((prev) => {
-//                 const newSlide = prev + direction;
-//                 return newSlide < 0 ? slides.length - 1 : newSlide % slides.length;
-//             });
-//             setTouchStartX(currentX); // сбрасываем начальную позицию
-//         }
-//     };
-//
-//     const handleMouseUp = () => {
-//         setIsDragging(false);
-//     };
-//
-//     const handleTouchStart = (e) => {
-//         preventScroll.current = true;
-//         setTouchStartX(e.touches[0].clientX);
-//         setTouchStartY(e.touches[0].clientY);
-//         setIsDragging(true);
-//         preventScroll.current = true; // включаем предотвращение прокрутки
-//     };
-//
-//     const handleTouchMove = (e) => {
-//         if (!isDragging) return;
-//         const currentX = e.touches[0].clientX;
-//         const diffX = currentX - touchStartX;
-//
-//         // Добавляем проверку на минимальное расстояние для смены слайда
-//         if (Math.abs(diffX) > 100) { // увеличиваем порог с 40 до 100
-//             const direction = diffX > 0 ? 1 : -1;
-//             setCurrentSlide((prev) => {
-//                 const newSlide = prev + direction;
-//                 return newSlide < 0 ? slides.length - 1 : newSlide % slides.length;
-//             });
-//             setTouchStartX(currentX);
-//         }
-//     };
-//
-//     const handleTouchEnd = () => {
-//         preventScroll.current = false;
-//         setIsDragging(false);
-//         preventScroll.current = false; // отключаем предотвращение прокрутки
-//     };
-//
-//     useEffect(() => {
-//         if(slideRef.current) {
-//             slideRef.current.style.transform = `translateX(-${currentSlide * 100}%)`;
-//         }
-//     }, [currentSlide]);
-//
-//     return (
-//         <div className={s.slider}
-//              onTouchStart={handleTouchStart}
-//              onTouchMove={handleTouchMove}
-//              onTouchEnd={handleTouchEnd}
-//              onMouseDown={handleMouseDown}
-//              onMouseMove={handleMouseMove}
-//              onMouseUp={handleMouseUp}
-//              onMouseLeave={handleMouseUp}
-//         >
-//             <div
-//                 className={s.slides}
-//                 ref={slideRef}
-//                 style={{
-//                     transform: `translateX(-${currentSlide * 100}%)`,
-//                     transition: 'transform 0.8s ease)'
-//                 }}
-//             >
-//                 {slides.map((slide, index) => (
-//                     <div
-//                         className={s.slide}
-//                         key={index}
-//                         style={{backgroundImage: `url(${slide.img})`}}
-//                     >
-//                         <div className={s.content}>
-//                             <h2>{slide.heading}</h2>
-//                         </div>
-//                     </div>
-//                 ))}
-//                 {slides.length > 0 && (
-//                     <div
-//                         className={s.slide}
-//                         style={{backgroundImage: `url(${slides[0].img})`}}
-//                     >
-//                         <div className={s.content}>
-//                             <h2>{slides[0].heading}</h2>
-//                         </div>
-//                     </div>
-//                 )}
-//             </div>
-//         </div>
-//     );
-// };
-//
-// export default Slider;
