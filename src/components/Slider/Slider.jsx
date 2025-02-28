657b5e730dfcfa4c7351f806a96a6c8be5143005
ryz <knyazevskaya.a.a@gmail.com>
2025-02-28 20:09:05 +0300

    add css for 2nd slide

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -15,12 +15,12 @@ const Slider = () => {
 
     const slides = [
         {img: bg1, heading: 'Привет,', block: (<span>Это <b>не</b><br/>коммерческое<br/>задание</span>), btn: 'Что дальше?'},
-        {img: bg2, heading: 'Текст сообщения', largeBlock: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis ut diam quam nulla, Mauris in aliquam sem fringilla ut morbi tincidunt. ' +
-                'Vitae aliquet nec ullamcorper sit amet risus nullam eget felis. Nulla pharetra diam sit amet nisl. Eget nulla facilisi etiam dignissim diam quis enim lobortis. Est sit amet facilisis magna. Neque laoreet suspend sse interdum consectet r libero id. ' +
-                'Nec ullamcorper sit amet risus nullam eget felis eget. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id Euismod quis viverra nibh cras pulvinar mattis nunc. Massa massa ultricies mi quis, Sit amet massa vitae tortor condimentum lacinia. ' +
-                'Et malesuada fames ac turpis egestas integer eget. Elementumm pulvinar etiam non quam lacus suspendisse faucibus interdum posuere.\n' +
-                'Amet justo donec enim diam vulputate ut pharetra sit, Risus ultricies tristique nulla aliquet enim tortor at auctor. Velit sed ullamcorper morbi tincidunt ornare massa. ' +
-                'Quis hendrerit dolor magna eget est lorem ipsum. Etiam dignissim diam quis enim. Gravida neque convallis a cras. Ut enim blandit volutpat maecenas volutpat. Mauris sit amet massa vitae tortor rondlimantum larinia nuis vel'},
+        {img: bg2, heading: 'Текст сообщения', largeBlock: (<span><b>Lorem ipsum dolor sit amet</b>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis ut diam quam nulla, Mauris in aliquam sem fringilla ut morbi tincidunt.
+                Vitae aliquet nec ullamcorper sit amet risus nullam eget felis. Nulla pharetra diam sit amet nisl. Eget nulla facilisi etiam dignissim diam quis enim lobortis. Est sit amet facilisis magna. Neque laoreet suspend sse interdum consectet r libero id.
+                Nec ullamcorper sit amet risus nullam eget felis eget. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id Euismod quis viverra nibh cras pulvinar mattis nunc. Massa massa ultricies mi quis, Sit amet massa vitae tortor condimentum lacinia.
+                Et malesuada fames ac turpis egestas integer eget. Elementumm pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. <br/><br/>
+                Amet justo donec enim diam vulputate ut pharetra sit, Risus ultricies tristique nulla aliquet enim tortor at auctor. Velit sed ullamcorper morbi tincidunt ornare massa.
+                Quis hendrerit dolor magna eget est lorem ipsum. Etiam dignissim diam quis enim. Gravida neque convallis a cras. Ut enim blandit volutpat maecenas volutpat. Mauris sit amet massa vitae tortor rondlimantum larinia nuis vel</span>)},
         {img: bg3, heading: 'Ключевое сообщение', smallBlock1: 'Enicula ipsum a arcu cursus vitae. Eu non diam phasellus vestibulum lorem sed risus ultricies', smallBlock2: 'A arcu cursus vitae'},
     ];
 
@@ -123,7 +123,13 @@ const Slider = () => {
                                 {slide.btn &&
                                     <button className={s.btn}><div className={s.arrow}>&rarr;</div><span>{slide.btn}</span></button>
                                 }
-                                <p>{slide.largeBlock}</p>
+                                {slide.largeBlock &&
+                                    <div className={s.largeBlock}>
+                                        <div className={s.text}>
+                                            {slide.largeBlock}
+                                        </div>
+                                    </div>
+                                }
                                 <p>{slide.smallBlock1}</p>
                                 <p>{slide.smallBlock2}</p>
                             </div>
