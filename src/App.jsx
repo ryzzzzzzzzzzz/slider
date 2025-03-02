53fcb6d5dd665763c1e27582e85ecd8501d34301
ryz <knyazevskaya.a.a@gmail.com>
2025-03-03 01:07:43 +0300

    improved html/css 3d slide

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Slider from "./components/Slider/Slider.jsx";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
 
   return (
     <>
+        <Header/>
         <Slider/>
     </>
   )
--- src/components/Slider/SlideContent/SlideContent.jsx
+++ src/components/Slider/SlideContent/SlideContent.jsx
@@ -1,18 +1,25 @@
 import PropTypes from 'prop-types';
 import s from "./style.module.css";
 import {useState} from "react";
-import ModalSlider from "../ModalSlider/ModalSlider.jsx";
 
 const SlideContent = ({ slide, index, goToSecondSlide }) => {
     const [isModalSliderOpen, setIsModalSliderOpen] = useState(false);
+    const [currentSlide, setCurrentSlide] = useState(0);
 
-    const controlModalSlider = (n) => {
-        setIsModalSliderOpen(n)
+    const controlModalSlider = (status) => {
+        setIsModalSliderOpen(status)
+    }
+
+    const leftSlide = () => {
+        setCurrentSlide(1)
+    }
+
+    const rightSlide = () => {
+        setCurrentSlide(0)
     }
 
     return (
         <>
-            {/*<h1>{slide.heading}</h1>*/}
             {index === 0 &&
                 <div className={s.first}>
                     <h1>{slide.heading}</h1>
@@ -34,28 +41,54 @@ const SlideContent = ({ slide, index, goToSecondSlide }) => {
                     <div className={s.frame}></div>
                 </div>
             }
-            {isModalSliderOpen && index === 2 &&
-                <ModalSlider controlModalSlider={controlModalSlider}/>
-            }
             {index === 2 &&
-                <div className={s.third}>
-                    <img className={s.bottle} src={slide.bottle} alt="bottle"/>
-                    <div className={s.head}>
-                        <h1>{isModalSliderOpen ? slide.modal : slide.heading}</h1>
-                        <h2>{slide.block}</h2>
-                        <div className={s.small}>
-                            <p>
-                                <img className={s.icon} src={slide.icon1} alt="icon1"/>
-                                {slide.smallBlock1}
-                            </p>
-                            <p>
-                                <img className={s.icon} src={slide.icon2} alt="icon2"/>
-                                {slide.smallBlock2}
-                            </p>
-                            <button className={s.btn} onClick={() => controlModalSlider(true)}>
-                                <div className={s.arrow}>+</div>
-                                {slide.btn}
-                            </button>
+                <div className={isModalSliderOpen ? s.overlay : ''}>
+                    <div className={s.third}>
+                        <img className={s.bottle} src={slide.bottle} alt="bottle"/>
+                        <div>
+                            <h1>{isModalSliderOpen ? slide.modal : slide.heading}</h1>
+                            <h2>{slide.block}</h2>
+                            {isModalSliderOpen ? (
+                                    <div className={s.mslider}>
+                                        <div  className={isModalSliderOpen ? s.modal : ''}>
+                                        <div className={s.close} onClick={() => controlModalSlider(false)}>×</div>
+                                            <ol className={s.left} style={{display: currentSlide === 1 ? 'none' : ''}}>
+                                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                                                <li>Faucibus pulvinar elementum integer enim</li>
+                                                <li>Faucibus pulvinar elementum integer enim</li>
+                                            </ol>
+                                            <ol className={s.right} style={{display: currentSlide === 0 ? 'none' : ''}}>
+                                                <li>Mi bibendum neque egestas congue quisque</li>
+                                                <li>Venenatis lectus magna fringilla urna</li>
+                                                <li>Venenatis lectus magna fringilla urna</li>
+                                            </ol>
+                                            <div className={s.nav}>
+                                                <div className={s.marrow} onClick={rightSlide}>&#60;</div>
+                                                <div className={`${s.dot} ${currentSlide === 0 ? s.active : ''}`}
+                                                     onClick={leftSlide}></div>
+                                                <div className={`${s.dot} ${currentSlide === 1 ? s.active : ''}`} onClick={rightSlide}></div>
+                                            <div className={s.marrow} onClick={leftSlide}>&#62;</div>
+                                        </div>
+                                    </div>
+                                    </div>
+                                )
+                                : (
+                                    <div className={s.small}>
+                                        <p>
+                                            <img className={s.icon} src={slide.icon1} alt="icon1"/>
+                                            {slide.smallBlock1}
+                                        </p>
+                                        <p>
+                                        <img className={s.icon} src={slide.icon2} alt="icon2"/>
+                                            {slide.smallBlock2}
+                                        </p>
+                                        <button className={s.btn} onClick={() => controlModalSlider(true)}>
+                                            <div className={s.arrow}>+</div>
+                                            {slide.btn}
+                                        </button>
+                                    </div>
+                                )
+                            }
                         </div>
                     </div>
                 </div>
